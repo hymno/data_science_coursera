477a44f8cc03dfc21b5ef0970fa8bb124cdc7c25
Adrian <adrian_rt@outlook.com>
2015-04-22 15:10:04 -0700

    commit april 22

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,4 +1,7 @@
-
+##This function gathers together all the data sets in a directory
+## of pollutant data sets into one data frame and computes
+## the mean of a set of pollutant data. For example, the mean of either
+## sulfate or nitrate levels.
 
 pollutantmean <- function(directory, pollutant, id = 1:332){
         data_files <- list.files(directory, full.names = TRUE)
